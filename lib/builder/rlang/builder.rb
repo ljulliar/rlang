d64e4a415f5d36a3fa9bb4cbb6537aafd8dd90ab
Laurent Julliard <laurent@moldus.org>
2020-01-07 10:26:43 +0100

    No need to check rlang. It's here by definition

--- lib/builder/rlang/builder.rb
+++ lib/builder/rlang/builder.rb
@@ -2,24 +2,19 @@
 # Copyright (c) 2019, Laurent Julliard and contributors
 # All rights reserved.
 
+require 'mkmf'
 require_relative '../ext/tempfile'
 
 module Builder::Rlang
   class Builder
 
-    # Rlang to WAT compiler
     RLANG_COMPILER = File.expand_path('../../../../bin/rlang', __FILE__)
 
     attr_reader :source, :target, :wat_path
 
     def initialize
-      check_compiler
       @wat_path = nil
       @target = nil
-    end
-
-    def check_compiler
-      raise "no rlang compiler found" unless File.exist? RLANG_COMPILER
     end
 
     def compile(source, target, options='')
