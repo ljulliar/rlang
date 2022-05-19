e783660ecc3335998296413d61872efbce70dae4
Laurent Julliard <laurent@moldus.org>
2022-05-19 22:03:04 +0200

    Add I32 string representation

--- lib/rlang/lib/type/i32.rb
+++ lib/rlang/lib/type/i32.rb
@@ -4,29 +4,37 @@
 #
 # Integer 32 methods
 
-class String; end
-
 class I32
-  ConvertString = "0123456789ABCDEF"
+
+  Digits = "0123456789ABCDEF"
 
   def self.size; 4; end
 
-  def to_str(base)
+  # convert an integer to its string representation in a given base
+  def self.itoa(x,base)
     result :String
-    "0"
-=begin
-    # TODO
-    if n < base
-      return convertString[n]
-    else
-      return toStr(n//base,base) + convertString[n%base]
+
+    raise "itoa base out of range" if base < 2 || base > Digits.length
+    if x <= 0
+      if x == 0
+        return Digits[0]
+      else
+        return  "-" + self.itoa(0-x, base)
+      end
     end
-=end
+
+    result = ""
+    while x > 0
+      remainder = x % base
+      x /= base
+      result += Digits[remainder]
+    end
+    result.reverse!
   end
 
   def to_s
     result :String
-    self.to_str(10)
+    I32.itoa(self, 10)
   end
 
 end
