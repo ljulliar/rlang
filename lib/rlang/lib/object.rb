d4e96c0495969e203d3e587027bd1fdfcbbca247
Laurent Julliard <laurent@moldus.org>
2022-06-07 19:19:06 +0200

    New Object methods added

--- lib/rlang/lib/object.rb
+++ lib/rlang/lib/object.rb
@@ -26,4 +26,27 @@ class Object
     result :String
     "Object <addr>"
   end
+
+  def object_id
+    result :I32
+    self
+  end
+
+  def eql?(object)
+    result :I32
+    inline wat: '(i32.eq 
+      (local.get $_self_)
+      (local.get $object))',
+           wtype: :I32,
+           ruby: 'self.object_id == object.object_id'
+  end
+
+  def ==(object)
+    self.eql?(object)
+  end
+
+  def !=(object)
+    !(self == object)
+  end
+
 end
