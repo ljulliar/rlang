9e81f1e4eb6fd1d35fedec5d4f8f637cc24f9e34
Laurent Julliard <laurent@moldus.org>
2022-06-07 19:05:24 +0200

    Now works for negative int too

--- lib/rlang/parser/ext/integer.rb
+++ lib/rlang/parser/ext/integer.rb
@@ -1,5 +1,7 @@
 class Integer
+  # Works for both signed and unsignd 32 or 64 bits integers
   def to_little_endian(byte_count)
-    ("%0#{byte_count*2}X" % self).scan(/../).reverse.map {|byte| "\\#{byte}"}.join('')
+    i = (self < 0) ? 2**(byte_count*8) + self : self
+    ("%0#{byte_count*2}X" % i).scan(/../).reverse.map {|byte| "\\#{byte}"}.join('')
   end
 end
